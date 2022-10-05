3ecbb33a01f3cb7662d1f3b127befbfdf56ae97a
Aaron <Harvey.AaronJ@gmail.com>
2022-10-05 14:20:49 -0400

    .

---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Test extends React.Component {
+	render () {
+		return <h1>This is a Test</h1>;
+	}
+}
+
+export default Test;
